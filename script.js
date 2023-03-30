1fca82c8d732f76938daa5db25997c09b3e179a1
YusufAkilevi <yusufaklevi@gmail.com>
2023-03-30 16:18:26 +0300

    mobile-nav added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const btnMobileNav = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+const containerEl = document.querySelector(".container");
+const mainNav = document.querySelector(".main-nav");
+
 btnMobileNav.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
